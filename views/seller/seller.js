0b262d8e265555a95e551029a81b5ab9f88cb224
NAVIN.CHHORN <NAVIN.CHHORN@PNC.LAN>
2022-11-29 20:23:32 +0700

    add new data

--- views/seller/seller.js
+++ views/seller/seller.js
@@ -2,12 +2,39 @@
 const dialog = document.querySelector("#dialog_product");
 const edit_btn = document.querySelector("#edit");
 const add_btn = document.querySelector("#add");
-const dialod_animal = document.querySelector("#animal").value;
-const dialog_description = document.querySelector("#description").value;
-const dialog_product =  document.querySelector("#cost").value;
+const Product_view = document.querySelector("#product-view");
 
 
-let animalData = []
+let animalData = [
+    {
+        name:"bird",
+        description:"black bird",
+        cost:"20$",
+        img:"https://images.pexels.com/photos/247502/pexels-photo-247502.jpeg?cs=srgb&dl=pexels-pixabay-247502.jpg&fm=jpg"
+        
+    },
+    {
+        name:"bird",
+        description:"black bird",
+        cost:"20$",
+        img:"https://images.pexels.com/photos/247502/pexels-photo-247502.jpeg?cs=srgb&dl=pexels-pixabay-247502.jpg&fm=jpg"
+        
+    },
+    {
+        name:"bird",
+        description:"black bird",
+        cost:"20$",
+        img:"https://images.pexels.com/photos/247502/pexels-photo-247502.jpeg?cs=srgb&dl=pexels-pixabay-247502.jpg&fm=jpg"
+        
+    },
+    {
+        name:"bird",
+        description:"black bird",
+        cost:"20$",
+        img:"https://images.pexels.com/photos/247502/pexels-photo-247502.jpeg?cs=srgb&dl=pexels-pixabay-247502.jpg&fm=jpg"
+        
+    },
+]
 
 
 // function hide element
@@ -45,16 +72,16 @@ function Oncreate(){
 
 function readerdata(){
     let animal = {}
-    animal.name = document.querySelector("#animal").value;
+    animal.name = document.querySelector("#nameOf_animal").value;
     animal.description = document.querySelector("#description").value;
     animal.cost = document.querySelector("#cost").value;
     animal.currency = document.querySelector("#currency").value;
-    // animal.id =( animalNews.length)+1;
-    animal.url = document.querySelector("#url").value;
+    animal.img = document.querySelector("#url").value;
 
+    console.log(animal.name)
     animalData.push(animal);
 
-    document.querySelector("#animal").value = "";
+    document.querySelector("#nameOf_animal").value = "";
     document.querySelector("#description").value= "";
     document.querySelector("#cost").value = "";
     document.querySelector("#currency").value = "";
@@ -72,19 +99,72 @@ function saveData() {
 function loadproduct() {
     let alldata = JSON.parse(localStorage.getItem("animalData"));
     console.log(alldata)
+    showProduct()
 
   
 }
 
 
-// function showData(){
-//     let table = document.querySelector("#product-container");
-    
-//     let 
-// }
+function showProduct(){
+    document.querySelector("#product-container").remove();
+    let product_container = document.createElement("div");
+    product_container.id ="product-container";
+    Product_view.appendChild(product_container);
+    for(i in animalData){
+        let card = document.createElement("div");
+        card.className = "card";
+        product_container.appendChild(card);
+
+        let img = document.createElement("img");
+        img.id = "animal";
+        img.src = animalData[i].img;
+        card.appendChild(img);
+
+        let retail_news = document.createElement("div");
+        retail_news.className = "retail_news";
+        card.appendChild(retail_news);
+
+        let news = document.createElement("div");
+        retail_news.appendChild(news);
+
+        let name = document.createElement("p");
+        name.className = "name_animal";
+        name.textContent = animalData[i].name;
+        news.appendChild(name)
+
+        let cost = document.createElement("p");
+        cost.textContent = animalData[i].cost;
+        news.appendChild(cost);
+
+        let span = document.createElement("span");
+        news.appendChild(span);
+
+        for (let i = 0 ; i<5 ; i++) {
+            let img = document.createElement("img");
+            img.className = "stars";
+            img.src = "../../images/black star.webp";
+            span.appendChild(img);
+        }
+
+        let action = document.createElement("div");
+        action.className = "action";
+        retail_news.appendChild(action);
+        let img_edit = document.createElement("img");
+        img_edit.src = "../../images/edit.png";
+        action.appendChild(img_edit);
+
+        let img_delete = document.createElement("img");
+        // img_delete.addEventListener(onclick,deleteData());
+        img_delete.src = "../../images/delete.png";
+        action.appendChild(img_delete);
+        
+    }
+}
 
 
-// showData()
+
+showProduct()
+
 
 
 
